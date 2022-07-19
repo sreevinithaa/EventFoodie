c406f05e28bc61bc1b72e497ea959564914b4ccb
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-20 04:13:45 +0800

    Update event model

--- server/models/Events.js
+++ server/models/Events.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const customerSchema = new Schema({
-});
-const Customer = mongoose.model('Customer', customerSchema);
+const eventSchema = new Schema({
 
-module.exports = Customer;
+    eventName: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      venue: {
+        type: String,
+        required: true
+      },
+      city: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      imageUrl: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      discription: {
+        type: String,
+        required: true
+      },
+      startDate: {
+        type: Date,
+        required: true
+      },
+      endDate: {
+        type: Date,
+        required: true
+      },
+});
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
