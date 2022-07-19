990dc2a06677df5ca5bfb2a34becb65b6661713d
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-20 04:34:03 +0800

    update order model

--- server/models/Menu.js
+++ server/models/Menu.js
@@ -2,8 +2,33 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const customerSchema = new Schema({
-});
-const Customer = mongoose.model('Customer', customerSchema);
+const menuSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      price:{
+        type: Number,
+        required: true
+      },
+      isAvailable:{
+        type:Boolean
 
-module.exports = Customer;
+      },
+      comboPrice:{
+        type: Number,
+        required: true
+      }
+});
+const Menu = mongoose.model('Menu', menuSchema);
+
+module.exports = Menu;
