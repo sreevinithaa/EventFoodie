a49c1e15e9af8b6ca4add001f3a1bd2c8aacfc04
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-07-21 18:48:00 +0800

    add auth.js file to the project

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -79,7 +79,7 @@ export default function SignUp() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray placeholder-gray text-gray rounded-xl focus:outline-none focus:ring-[#662B6D] focus:border-[#662B6D] focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-gray placeholder-gray text-gray rounded-xl focus:outline-none focus:ring-[#662B6D] focus:border-[#662B6D] focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
